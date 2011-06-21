72af1a17f2652d7f33120504f2aee6ba75326da4
Nitay Joffe <n@fb.com>
2011-06-20 20:02:33 -0700

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+class etckeeper {
+  case $operatingsystem {
+    fedora: {
+      $highlevel_package_manager = "yum"
+      $lowlevel_package_manager ="rpm"
+    }
+    ubuntu: {
+      $highlevel_package_manager = "apt"
+      $lowlevel_package_manager ="dpkg"
+    }
+    default: { fail("Don't know how to handle ${operatingsystem}") }
+  }
+
+  package { etckeeper: ensure => installed; }
+
+  file {
+    "/etc/etckeeper/etckeeper.conf":
+      ensure => present,
+      content => template("etckeeper/etckeeper.conf.erb"),
+      require => Package["etckeeper"];
+  }
+
+  Exec { path => ["/usr/bin", "/usr/sbin"] }
+
+  exec {
+    "etckeeper_init":
+      command => "etckeeper init",
+      creates => "/etc/.git",
+      require => File["/etc/etckeeper/etckeeper.conf"];
+  }
+}
