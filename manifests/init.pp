cedd088dc3107ff2fdb24bcbbbb93a56c3381994
Daniel Steudler <daniel.steudler@gmx.ch>
2012-06-11 13:55:26 +0200

    Corrected indenting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,34 +1,34 @@
 class etckeeper {
-  case $operatingsystem {
-    fedora: {
-      $highlevel_package_manager = "yum"
-      $lowlevel_package_manager ="rpm"
+    case $operatingsystem {
+        fedora: {
+            $highlevel_package_manager = "yum"
+            $lowlevel_package_manager ="rpm"
+        }
+        ubuntu: {
+            $highlevel_package_manager = "apt"
+            $lowlevel_package_manager ="dpkg"
+        }
+        default: { fail("Don't know how to handle ${operatingsystem}") }
     }
-    ubuntu: {
-      $highlevel_package_manager = "apt"
-      $lowlevel_package_manager ="dpkg"
+
+    if ! defined (Package["etckeeper"]) {
+        package { "etckeeper": ensure => installed; }
     }
-    default: { fail("Don't know how to handle ${operatingsystem}") }
-  }
-
-  if ! defined (Package["etckeeper"]) {
-       package { "etckeeper": ensure => installed; }
-  }
 
 
-  file {
-    "/etc/etckeeper/etckeeper.conf":
-      ensure => present,
-      content => template("etckeeper/etckeeper.conf.erb"),
-      require => Package["etckeeper"];
-  }
+    file {
+        "/etc/etckeeper/etckeeper.conf":
+            ensure => present,
+            content => template("etckeeper/etckeeper.conf.erb"),
+            require => Package["etckeeper"];
+    }
 
-  Exec { path => ["/usr/bin", "/usr/sbin"] }
+    Exec { path => ["/usr/bin", "/usr/sbin"] }
 
-  exec {
-    "etckeeper_init":
-      command => "etckeeper init",
-      creates => "/etc/.git",
-      require => File["/etc/etckeeper/etckeeper.conf"];
-  }
+    exec {
+        "etckeeper_init":
+            command => "etckeeper init",
+            creates => "/etc/.git",
+            require => File["/etc/etckeeper/etckeeper.conf"];
+    }
 }
